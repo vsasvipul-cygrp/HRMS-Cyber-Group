8573ce10cd462e9169c5bbee00ffd51ef1dcdbe8
initishsharma <nitish.sharma@cygrp.com>
2021-04-29 13:22:17 +0530

    #3 Added Javascript for login JWT
    Next step is to make Employee controller.

---
+++ HRMS_PROJECT/script.js
@@ -0,0 +1,45 @@
+function login()
+{
+ var EmailId = document.getElementById("email");
+ var Password = document.getElementById("pwd");
+
+var LoginUser = 
+{
+"emailid":EmailId.value,
+"password":Password.value,
+}
+
+fetch("https://localhost:44315/api/login", 
+{   
+    method: "POST",
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer',
+    body: JSON.stringify(LoginUser),
+    
+})
+//.then(response => response.json())
+.then((response) => response.text())
+  .then((response) => {
+    console.log("Bearer " + response);
+    window.localStorage.setItem("token", response);
+    if (window.localStorage.getItem("token").toString() != "false") {
+      alert("Login Successful. Open Link");
+      openPage();
+    } else {
+      alert("This user does not exist");
+    }
+  });
+
+}
+
+function openPage(){
+  window.open("./index.html", "_blank");
+}
