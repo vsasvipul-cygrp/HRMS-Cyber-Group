8348440008fbd9dadfe76a6ac433b4f2284e483f
magg99 <mayank.aggarwal@cygrp.com>
2021-05-06 10:57:23 +0530

    Updated UI Files

---
+++ HRMS UI and FrontEnd/HRMS/MANAGER_EMPLOYEE/signin-manage.js
@@ -0,0 +1,102 @@
+
+  
+    // ----------- ADD USER FUNCTION IS HERE //// 
+  
+  
+    function AddEmployee(){
+      var EmployeeName = document.getElementById("empname");
+      var EmployeeEmailId = document.getElementById("empemailid");
+      var EmployeePassword = abc();
+      var EmployeeContact = document.getElementById("empcontact");    
+      var e = document.getElementById("empdesg");
+      var EmployeeDesg = e.options[e.selectedIndex].text;
+      var x = document.getElementById("managernamelist");
+      var EmployeeAM = x.options[x.selectedIndex].text;
+      
+      var EmployeeRole=null;
+      if(EmployeeDesg=='Manager') EmployeeRole=2;
+      else EmployeeRole=3;
+  
+      var tempUser = {
+        "empname": EmployeeName.value,
+        "emailid": EmployeeEmailId.value,
+        "password": EmployeePassword,
+        "desg": EmployeeDesg,
+        "contact": EmployeeContact.value,      
+        "amid": EmployeeAM,
+        "role": EmployeeRole
+      };
+      console.log(tempUser);
+      fetch("https://localhost:44315/api/employee", 
+      {
+           
+    
+        method: "POST",
+        mode: "cors", // no-cors, *cors, same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+          "Content-Type": "application/json",
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: "follow", // manual, *follow, error
+        referrerPolicy: "no-referrer",
+        body: JSON.stringify(tempUser),
+      })
+  
+      console.log("New Account Added");
+      
+    
+    }
+  
+    function abc(){
+      console.log("aagaya yaha")
+      var a=document.getElementById("emppass");
+      var md5Hash = CryptoJS.MD5(a.value); 
+      
+      console.log(md5Hash.toString()); 
+      return md5Hash.toString();
+      }
+  
+  
+    // -------------- FETCH DATA FROM EMPLOYEE TABLE ------------ 
+  
+    function EmployeeFetchData() {
+      fetch("https://localhost:44315/api/employee", {
+        mode: "cors", // no-cors, *cors, same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+          "Content-Type": "application/json",
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: "follow", // manual, *follow, error
+        referrerPolicy: "no-referrer",
+      })
+        .then((res) => res.json())
+    
+        .then((data) => {
+          
+    
+          // console.log(data);
+          let li = ``;
+          data.forEach((EmployeeFetch) => {
+            // console.log(EmployeeFetch);
+            li += `<tr>
+                  
+                  <td>${EmployeeFetch.empname}</td>
+                  <td>${EmployeeFetch.emailid} </td>
+                  <td>${EmployeeFetch.desg}</td>
+                  <td>${EmployeeFetch.contact}</td>
+                  <td>${EmployeeFetch.amid}</td>
+                             
+                  </tr>`;
+          });
+          document.getElementById("employeedata").innerHTML = li;          
+        })
+        .catch(function (error) {
+    
+          console.log("Looks like there was a problem: \n", error);
+        });   
+        
+    }
---
+++ HRMS UI and FrontEnd/README.md
@@ -0,0 +1,1 @@
+# HRMS-Cyber-Group-Inc.
