fc7f1d8b268ba8e844505352586d87a76c75c589
magg99 <mayank.aggarwal@cygrp.com>
2021-05-07 13:31:16 +0530

    HRMS Front End

---
+++ HRMS UI and FrontEnd/HRMS/ADMIN/managerusers.js
@@ -0,0 +1,323 @@
+// ****************************************************
+// *********************FETCH DATA*********************
+// ****************************************************
+function EmployeeFetchData() {
+    fetch("https://localhost:44315/api/employee", {
+        mode: "cors", // no-cors, *cors, same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+            "Content-Type": "application/json",
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: "follow", // manual, *follow, error
+        referrerPolicy: "no-referrer",
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            let li = ``;
+            data.forEach((e) => {
+                li += `<tr>                  
+                    <td>${e.empname}</td>
+                    <td>${e.emailid} </td>
+                    <td>${e.desg}</td>
+                    <td>${e.contact}</td>
+                    <td>${e.amid}</td>    
+                   
+                    <td> <a href="#!" onclick="changeStatus(${e.id},'${e.empname}','${e.emailid}','${e.password}','${e.desg}','${e.contact}','${e.amid}',${e.role},'${e.isActive}')" class="label theme-bg text-white f-12 rounded" data-toggle="modal" data-target="#update2" >Update </a></td>         
+                                       
+                  </tr>`;
+            });
+            document.getElementById("currentemployeedata").innerHTML = li;
+        })
+        .catch(function (error) {
+            console.log("Looks like there was a problem: \n", error);
+        });
+
+
+// Past employyeee data
+fetch("https://localhost:44315/api/employee/past", {
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer",
+})
+    .then((res) => res.json())
+    .then((data) => {
+        let li = ``;
+        data.forEach((e) => {
+            li += `<tr>                  
+                <td>${e.empname}</td>
+                <td>${e.emailid} </td>
+                <td>${e.desg}</td>
+                <td>${e.contact}</td>
+                <td>${e.amid}</td>    
+               
+                <td> <a href="#!" onclick="changeStatus(${e.id},'${e.empname}','${e.emailid}','${e.password}','${e.desg}','${e.contact}','${e.amid}',${e.role},'${e.isActive}')" class="label theme-bg text-white f-12 rounded" data-toggle="modal" data-target="#update2" >Update </a></td>                                    
+              </tr>`;
+        });
+        document.getElementById("pastemployeedata").innerHTML = li;
+    })
+    .catch(function (error) {
+        console.log("Looks like there was a problem: \n", error);
+    });
+
+
+
+
+
+
+
+
+}
+
+//var e,x,y,EmployeeDesg,Amid,Active;
+
+function changeStatus(id, name, email, pwd, des, contact, amid, role, status) {
+    window.localStorage.setItem("PatchID",id);
+    console.log(contact);
+    document.getElementById("emp_name").value=name;
+   document.getElementById("emp_emailid").value=email;
+   document.getElementById("emp_contact").value=contact;
+   
+
+   
+
+   
+    // let newStatus;
+    // if (status == "Active") newStatus = "InActive"
+    // else newStatus = "Active"
+    //
+
+    //window();
+
+    //window.open('./manage_users.html','_self')
+
+
+}
+function update()
+{
+    var name=document.getElementById("emp_name").value;
+    var emailid=document.getElementById("emp_emailid").value;
+    var contact=document.getElementById("emp_contact").value;
+    var id=window.localStorage.getItem("PatchID");
+    
+
+
+   const e = document.getElementById("emp_desg");
+   const EmployeeDesg = e.options[e.selectedIndex].text;
+   const x = document.getElementById("manager_namelist");
+   const  Amid = x.options[x.selectedIndex].text;
+    const y = document.getElementById("activeList");
+    const Active = y.options[y.selectedIndex].text;
+   
+var role;
+if(EmployeeDesg=="Manager"){
+    role=2;
+}
+else{
+
+    role=3;
+}
+var Employee = {
+        "empname": name,
+        "emailid": emailid,
+        "desg": EmployeeDesg,
+        "contact": contact,
+        "amid": Amid,
+        "role": role,
+        "isActive": Active
+    };
+    console.log(Employee);
+    fetch("https://localhost:44315/api/employee/" + id.toString(), {
+        method: "PATCH",
+        mode: "cors", // no-cors, *cors, same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+            "Content-Type": "application/json",
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: "follow", // manual, *follow, error
+        referrerPolicy: "no-referrer",
+        body: JSON.stringify(Employee),
+    })
+        .then(response => response.json())
+        .then((result) => {
+            console.log(result);
+            EmployeeFetchData();
+
+     });
+
+
+}
+
+EmployeeFetchData();
+
+// ****************************************************
+// ****************FORM VALIDATION*********************
+// ****************************************************
+const form = document.getElementById('form');
+const username = document.getElementById('empname');
+const email = document.getElementById('empemailid');
+const password = document.getElementById('emppass');
+const contact = document.getElementById('empcontact');
+const e = document.getElementById("empdesg");
+const x = document.getElementById("managernamelist");
+
+function checkInputs() {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const contactValue = contact.value.trim();
+    const EmployeeDesg = e.options[e.selectedIndex].text;
+    const EmployeeAM = x.options[x.selectedIndex].text;
+
+    if (usernameValue === '') setErrorFor(username, 'Employee Name cannot be blank');
+    else {
+        setSuccessFor(username);
+        if (emailValue === '') setErrorFor(email, 'Email cannot be blank');
+        else if (!isEmail(emailValue)) setErrorFor(email, 'Email is not valid');
+        else {
+            setSuccessFor(email);
+            if (passwordValue === '') setErrorFor(password, 'Password cannot be blank');
+            else if (!isPassword(passwordValue)) setErrorFor(password, 'Minimum 8 characters, 1 uppercase & 1 lowercase letter, 1 number & 1 special character');
+            else {
+                setSuccessFor(password);
+                if (contactValue === '') setErrorFor(contact, 'Phone Number cannot be blank');
+                else if (!isPhone(contactValue)) setErrorFor(contact, 'Phone Number is not valid');
+                else {
+                    setSuccessFor(contact);
+                    if (EmployeeDesg === '' || EmployeeDesg == 'Select Designation') setErrorFor(e, 'Designantion cannot be blank');
+                    else {
+                        setSuccessFor(e);
+                        if (EmployeeAM === '' || EmployeeAM == 'Select Appointed Manager') setErrorFor(x, 'Appointed Manager cannot be blank');
+                        else {
+                            setSuccessFor(x);
+                            AddEmployee();
+                            $('#staticBackdrop').modal('hide')
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+}
+
+function clearModal() {
+
+    $('#staticBackdrop').on('hidden.bs.modal', function () {
+        $(this).find('form').trigger('reset');
+    })
+}
+
+
+
+
+// ****************************************************
+// *************ADD USER FORM VALIDATION****************
+// ****************************************************
+function setErrorFor(input, message) {
+
+    const formCtrl = input.parentElement; //.form-control
+
+    const small = formCtrl.querySelector('small');
+    console.log(formCtrl)
+
+    console.log(small)
+
+    //add error message inside small
+    small.innerText = message;
+
+    //add error class
+    formCtrl.className = 'form-ctrl error';
+}
+function setSuccessFor(input) {
+    const formCtrl = input.parentElement; //.form-control
+    formCtrl.className = 'form-ctrl success';
+}
+
+function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+function isPhone(contact) {
+    return /^(\d{10})|(\d{3}\s\d{3}\s\d{4})|(\d{3}\.\d{3}\.\d{4})|(\d{3}-\d{3}-\d{4})|(\d{3}\.\d{3}\.\d{4})|(\(\d{3}\)(\.|\s|)\d{3}\.\d{4})|(\(\d{3}\)(-|\s|)\d{3}-\d{4})|(\(\d{3}\)\s\d{3}\s\d{4})$/.test(contact);
+}
+
+function isPassword(password) {
+    return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(password);
+}
+// ****************************************************
+// *********************ADD DATA*********************
+// ****************************************************
+function AddEmployee() {
+    var EmployeeName = document.getElementById("empname");
+    var EmployeeEmailId = document.getElementById("empemailid");
+    var EmployeePassword = abc();
+    var EmployeeContact = document.getElementById("empcontact");
+    var e = document.getElementById("empdesg");
+    var EmployeeDesg = e.options[e.selectedIndex].text;
+    var x = document.getElementById("managernamelist");
+    var EmployeeAM = x.options[x.selectedIndex].text;
+
+    var EmployeeRole = null;
+    if (EmployeeDesg == 'Manager') EmployeeRole = 2;
+    else EmployeeRole = 3;
+
+    var tempUser = {
+        "empname": EmployeeName.value,
+        "emailid": EmployeeEmailId.value,
+        "password": EmployeePassword,
+        "desg": EmployeeDesg,
+        "contact": EmployeeContact.value,
+        "amid": EmployeeAM,
+        "role": EmployeeRole,
+        "IsActive":"Active"
+    };
+    console.log(tempUser);
+    fetch("https://localhost:44315/api/employee",
+        {
+            method: "POST",
+            mode: "cors", // no-cors, *cors, same-origin
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "same-origin", // include, *same-origin, omit
+            headers: {
+                "Content-Type": "application/json",
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: "follow", // manual, *follow, error
+            referrerPolicy: "no-referrer",
+            body: JSON.stringify(tempUser),
+        })
+        .then(response => response.json())
+        .then((result) => {
+            console.log("upar")
+            EmployeeFetchData();
+            //window.location.reload();
+            console.log("niche")
+
+        });
+    
+
+}
+// ****************************************************
+// ***********PASSWORD ENCRYPTION*********************
+// ****************************************************
+function abc() {
+    var a = document.getElementById("emppass").value;
+    const key = '55a51621a6648525';
+    const keyutf = CryptoJS.enc.Utf8.parse(key);
+    const iv = CryptoJS.enc.Base64.parse(key);
+    const enc = CryptoJS.AES.encrypt(a, keyutf, { iv: iv });
+    const encStr = enc.toString();
+    console.log('encStr', encStr);
+    return encStr;
+}
---
+++ HRMS UI and FrontEnd/HRMS/MANAGER_EMPLOYEE/manager_leave_request.js
@@ -0,0 +1,244 @@
+var role = localStorage.getItem("role")
+var AmId = localStorage.getItem("Employee-name")
+
+if (role != 2) document.getElementById("manager").style.display = 'none'
+fetchLeaveRequests();
+function fetchLeaveRequests() {
+    fetch("https://localhost:44315/api/leave/" + AmId,
+        {
+            method: "GET",
+            mode: "cors", // no-cors, *cors, same-origin
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "same-origin", // include, *same-origin, omit
+            headers: {
+                "Content-Type": "application/json",
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: "follow", // manual, *follow, error
+            referrerPolicy: "no-referrer",
+        })
+        //.then(response => response.json())
+        .then((result) => result.json())
+
+        .then((data) => {
+
+
+            console.log(data);
+            let li = ``;
+            data.forEach((EmployeeFetch) => {
+
+                console.log(EmployeeFetch);
+
+                var sdate = `${EmployeeFetch.sdate}`;
+                var yyyy = sdate.slice(0, 4)
+                var mm = sdate.slice(5, 7)
+                var dd = sdate.slice(8, 10)
+                sdate = mm + "/" + dd + "/" + yyyy
+
+                var edate = `${EmployeeFetch.edate}`;
+                var yyyy = edate.slice(0, 4)
+                var mm = edate.slice(5, 7)
+                var dd = edate.slice(8, 10)
+                edate = mm + "/" + dd + "/" + yyyy
+
+                var reason = `${EmployeeFetch.reason}`;
+                reason = reason.replace("'", "~");
+                console.log(reason)
+                li += `<tr>
+              
+              <td>${EmployeeFetch.id}</td>
+              <td>${EmployeeFetch.empname} </td>
+              <td>${sdate}</td>
+              <td>${edate}</td>
+              <td>${EmployeeFetch.reason}</td>
+              <td>${EmployeeFetch.type}</td>
+                <td> <a href="#!" onclick="approveRequest('${EmployeeFetch.empname}',${EmployeeFetch.leaveid},${EmployeeFetch.id},${EmployeeFetch.typeid},'${EmployeeFetch.sdate}','${EmployeeFetch.edate}','${reason}')" class="label theme-bg text-white f-12 rounded">Approve</a> <a href="#!" onclick="rejectRequest('${EmployeeFetch.empname}',${EmployeeFetch.leaveid},${EmployeeFetch.id},${EmployeeFetch.typeid},'${EmployeeFetch.sdate}','${EmployeeFetch.edate}','${reason}')" class="label theme-bg2 text-white f-12 rounded">Reject</a></td>         
+                
+                </tr>`;
+
+            });
+            document.getElementById("managerLeaveRequests").innerHTML = li;
+        });
+}
+
+
+function approveRequest(name, lId, eId, typeId, sDate, eDate, reason) {
+    console.log(name)
+    console.log(lId)
+    console.log(eId)
+    console.log(typeId)
+    console.log(sDate)
+    console.log(eDate)
+    reason = reason.replace("~", "'");
+    console.log(reason)
+    var Leave = {
+        "id": eId,
+        "typeid": typeId,
+        "sdate": sDate,
+        "edate": eDate,
+        "reason": reason,
+        "status": "Approved",
+    };
+    console.log(Leave);
+    var urlUpdate = "https://localhost:44315/api/leave/" + lId.toString();
+    console.log(urlUpdate);
+    fetch(urlUpdate, {
+        method: "PUT",
+        mode: "cors", // no-cors, *cors, same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+            "Content-Type": "application/json",
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: "follow", // manual, *follow, error
+        referrerPolicy: "no-referrer",
+        body: JSON.stringify(Leave),
+    })
+        .then(response => response.json())
+        .then((result) => {
+            fetchLeaveRequests();
+        });
+
+    fetch("https://localhost:44315/api/employee/getmanageremailid/" + name.toString(), {
+        method: "GET",
+        mode: "cors", // no-cors, *cors, same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+            "Content-Type": "application/json",
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: "follow", // manual, *follow, error
+        referrerPolicy: "no-referrer",
+        // body: JSON.stringify(Leave),
+    })
+        .then(response => response.json())
+        .then((result) => {
+
+            var email = result[0].emailid;
+
+            var Body = 'Hey ' + "<b>" + name + "</b>" + ', Your Leave has been <b>Approved.</b>';
+
+
+            Email.send({
+                SecureToken: "15310dfc-5ba6-423d-8644-4b455b088f7c",
+                To: email,
+                From: "hrmscygrp@gmail.com",
+                Subject: "Leave Approved",
+                Body: Body
+            }).then(
+                message => {
+                    //console.log (message);
+                    if (message == 'OK') {
+                        swal({
+                            icon: 'success',
+                            text: 'Leave Approved Successfully',
+                        });
+                    }
+                    else {
+                        console.error(message);
+                        swal({
+                            icon: 'error',
+                            title: 'Oops...',
+                            text: 'Leave Approval Failed',
+
+                        });
+
+                    }
+
+                }
+            );
+        });
+}
+
+function rejectRequest(name, lId, eId, typeId, sDate, eDate, reason) {
+    console.log(lId)
+    console.log(eId)
+    console.log(typeId)
+    console.log(sDate)
+    console.log(eDate)
+    reason = reason.replace("~", "'");
+    console.log(reason)
+    var Leave = {
+        "id": eId,
+        "typeid": typeId,
+        "sdate": sDate,
+        "edate": eDate,
+        "reason": reason,
+        "status": "Rejected",
+    };
+    console.log(Leave);
+    var urlUpdate = "https://localhost:44315/api/leave/" + lId.toString();
+    console.log(urlUpdate);
+    fetch(urlUpdate, {
+        method: "PUT",
+        mode: "cors", // no-cors, *cors, same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+            "Content-Type": "application/json",
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: "follow", // manual, *follow, error
+        referrerPolicy: "no-referrer",
+        body: JSON.stringify(Leave),
+    })
+        //.then(response => response.json())
+        .then((result) => {
+            fetchLeaveRequests();
+        });
+    var url2 = "https://localhost:44315/api/employee/getmanageremailid/" + name.toString();
+
+    fetch(url2, {
+        method: "GET",
+        mode: "cors", // no-cors, *cors, same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+            "Content-Type": "application/json",
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: "follow", // manual, *follow, error
+        referrerPolicy: "no-referrer",
+        // body: JSON.stringify(Leave),
+    })
+        .then(response => response.json())
+        .then((result) => {
+
+            var email = result[0].emailid;
+
+            var Body = 'Hey ' + "<b>" + name + "</b>" + ', Your Leave has been <b>Rejected.</b>';
+
+
+            Email.send({
+                SecureToken: "15310dfc-5ba6-423d-8644-4b455b088f7c",
+                To: email,
+                From: "hrmscygrp@gmail.com",
+                Subject: "Leave Rejected",
+                Body: Body
+            }).then(
+                message => {
+                    //console.log (message);
+                    if (message == 'OK') {
+                        swal({
+                            icon: 'success',
+                            text: 'Leave Rejected Successfully',
+                        });
+                    }
+                    else {
+                        console.error(message);
+                        swal({
+                            icon: 'error',
+                            title: 'Oops...',
+                            text: 'Leave Rejection Failed',
+
+                        });
+
+                    }
+
+                }
+            );
+        });
+}
+
